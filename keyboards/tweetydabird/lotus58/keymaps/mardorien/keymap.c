a37f266f4a709953369be2d7c9b37f9e571b050b
ezri.munro@pm.me <ezri.munro@pm.me>
2026-01-18 11:29:44 +0000

    unicode £ added to keymap; some additional stuff going on

--- keyboards/tweetydabird/lotus58/keymaps/mardorien/keymap.c
+++ keyboards/tweetydabird/lotus58/keymaps/mardorien/keymap.c
@@ -1,12 +1,55 @@
 // Copyright 2022 Markus Knutsson (@TweetyDaBird)
 // SPDX-License-Identifier: GPL-2.0-or-later
+// Major adjustments and additions by Mardorien (bsky: @mxezrirobyn.net)
 #include QMK_KEYBOARD_H
+#define SFTLLCK LSFT_T(KC_0)
+#define RALTLCK ALGR_T(KC_0)
+
+#if defined(TAP_DANCE_ENABLE)
+// Tap Dance declarations
+enum {
+    TD_ESC_GV,
+};
+
+// Tap Dance definitions
+tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Escape, twice for Caps Lock
+    [TD_ESC_GV] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_GRV),
+};
+#endif
+
+#if defined(UNICODEMAP_ENABLE)
+
+enum unicode_names {
+    GBP,
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+    [GBP] = 0x00A3, // £
+};
+
+#endif
+
+#if defined(COMBO_ENABLE)
+
+enum combo_events {
+  COMBO_GBP,
+};
+
+const uint16_t PROGMEM gbp_combo[] = {KC_HASH, KC_DLR, COMBO_END};
+
+combo_t key_combos[] = {
+    [COMBO_GBP] = COMBO_ACTION(gbp_combo),
+};
+
+#endif
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Linux
     [0] = LAYOUT(
         XXXXXXX,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_MPLY,        KC_MPLY,  KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,
-        QK_GESC,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                             KC_J,    KC_L,    KC_U,    KC_Y,   KC_SCLN, KC_BSLS,
+    TD(TD_ESC_GV),KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                             KC_J,    KC_L,    KC_U,    KC_Y,   KC_SCLN, KC_BSLS,
          KC_TAB,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                             KC_M,    KC_N,    KC_E,    KC_I,   KC_O,    KC_QUOT,
         LCTL_T(KC_LEFT), KC_Z, KC_X,  KC_C,  KC_D,    KC_V,   CW_TOGG,        AS_TOGG,  KC_K,    KC_H,    KC_COMM, KC_DOT, KC_SLSH, RCTL_T(KC_RGHT),
                                KC_LGUI, MO(3),   KC_LALT, LSFT_T(KC_SPC), RSFT_T(KC_ENT),   KC_RALT, MO(4), RGUI_T(KC_BSPC)
@@ -15,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Windows
     [1] = LAYOUT(
         XXXXXXX,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_MPLY,        KC_MPLY,  KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,
-        QK_GESC,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                             KC_J,    KC_L,    KC_U,    KC_Y,   KC_SCLN, KC_BSLS,
+        QK_GESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                             KC_J,    KC_L,    KC_U,    KC_Y,   KC_SCLN, KC_BSLS,
          KC_TAB,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                             KC_M,    KC_N,    KC_E,    KC_I,   KC_O,    KC_QUOT,
         LCTL_T(KC_LEFT), KC_Z, KC_X,  KC_C,  KC_D,    KC_V,   CW_TOGG,        AS_TOGG,  KC_K,    KC_H,    KC_COMM, KC_DOT, KC_SLSH, RCTL_T(KC_RGHT),
                               KC_LWIN, MO(3),   KC_LALT, LSFT_T(KC_SPC), RSFT_T(KC_ENT),   KC_RALT, MO(4), RWIN_T(KC_BSPC)
@@ -24,28 +67,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Game
     [2] = LAYOUT(
         XXXXXXX,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,  KC_MPLY,        KC_MPLY, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        QK_GESC,    KC_T,    KC_Q,    KC_W,    KC_E,    KC_R,                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+        QK_GESC,   KC_T,    KC_Q,    KC_W,    KC_E,    KC_R,                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
          KC_TAB,   KC_G,    KC_A,    KC_S,    KC_D,    KC_F,                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         LCTL_T(KC_LEFT), KC_Z, KC_X, KC_C,    KC_V,    KC_B,  KC_CAPS,        AS_TOGG, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RCTL_T(KC_RGHT),
                               KC_LGUI, MO(3),  KC_LALT, LSFT_T(KC_SPC), RSFT_T(KC_ENT),   KC_RALT, MO(4), RGUI_T(KC_BSPC)
     ),
 
+    // Numpad & arrow keys
     [3] = LAYOUT(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,        _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  XXXXXXX,
-         KC_TAB, KC_LPRN, KC_UP,   KC_RPRN, KC_CIRC, XXXXXXX,                          KC_HASH, KC_KP_7, KC_KP_8, KC_KP_9, KC_PDOT, XXXXXXX,
+         KC_TAB, KC_LPRN, KC_UP,   KC_RPRN, KC_CIRC, XXXXXXX,                          UM(GBP), KC_KP_7, KC_KP_8, KC_KP_9, KC_PDOT, XXXXXXX,
          KC_DEL, KC_LEFT, KC_DOWN, KC_RIGHT,KC_LBRC, KC_RBRC,                          KC_AMPR, KC_KP_4, KC_KP_5, KC_KP_6, KC_COLN, XXXXXXX,
         _______, KC_PMNS, KC_PAST, KC_PPLS, KC_PSLS, KC_EQL,   KC_NUM,        _______, KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3, KC_DLR,  _______,
-                                   _______, _______, _______, _______,        _______, _______, _______, _______
+                                   _______, _______, _______, SFTLLCK,        _______, RALTLCK, _______, _______
     ),
 
+    // Numline symbols & funcs
     [4] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______, _______,
         _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,                          _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4, _______,
         _______, KC_EXLM,   KC_AT, KC_HASH, KC_DLR,  KC_PERC,                          _______,   KC_F5,   KC_F6,   KC_F7,   KC_F8, _______,
         _______, KC_MINS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_CAPS,        _______, _______,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______,
-                                    OSL(5), _______, _______, _______,        _______, _______, _______, _______
+                                    OSL(5), _______, _______, SFTLLCK,        _______, RALTLCK, _______, _______
     ),
 
+    // System functions
     [5] = LAYOUT(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, NK_TOGG, AC_TOGG,                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
@@ -87,6 +133,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                         break;
                 }
                 return false;
+                break;
             }
         case PB_32:
             if (record->event.pressed) {
@@ -106,10 +153,30 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                         break;
                 }
                 return false;
+                break;
             }
+        case SFTLLCK:
+            if (record->tap.count) {
+                if (record->event.pressed) {
+                    // Toggle the lock on the highest layer.
+                    layer_lock_invert(get_highest_layer(layer_state));
+                }
+                return false;
+            }
+            break;
+        case RALTLCK:
+            if (record->tap.count) {
+                if (record->event.pressed) {
+                    // Toggle the lock on the highest layer.
+                    layer_lock_invert(get_highest_layer(layer_state));
+                }
+                return false;
+            }
+            break;
         default:
             return true;
     }
+    return true;
 }
 
 #if defined(OS_DETECTION_ENABLE)
@@ -123,12 +190,15 @@ bool process_detected_host_os_kb(os_variant_t detected_os) {
         case OS_IOS:
         case OS_WINDOWS:
             set_single_default_layer(1);
+            set_unicode_input_mode(UNICODE_MODE_WINDOWS);
             break;
         case OS_LINUX:
             set_single_default_layer(0);
+            set_unicode_input_mode(UNICODE_MODE_LINUX);
             break;
         case OS_UNSURE:
             set_single_default_layer(0);
+            set_unicode_input_mode(UNICODE_MODE_LINUX);
             break;
     }
 
@@ -184,9 +254,12 @@ static void print_layers(void) {
     } else {
         oled_write_P(PSTR("-Game"), true);
     }
-    oled_write_P(PSTR("|Numb"), IS_LAYER_ON(3));
-    oled_write_P(PSTR("|Symb"), IS_LAYER_ON(4));
-    oled_write_P(PSTR("|Syst"), IS_LAYER_ON(5));
+    oled_write_P(PSTR("|"), IS_LAYER_ON(3));
+    oled_write_P(PSTR("Numb"), is_layer_locked(3));
+    oled_write_P(PSTR("|"), IS_LAYER_ON(4));
+    oled_write_P(PSTR("Symb"), is_layer_locked(4));
+    oled_write_P(PSTR("|"), IS_LAYER_ON(5));
+    oled_write_P(PSTR("Syst"), is_layer_locked(5));
     oled_write_P(PSTR("     "), false);
         if (default_layer_state == 1) {
             oled_write_P(PSTR(" / \\ "), false);
