7eb29096de6788e69f031820e4ae3b5a9045a89e
ezri.munro@pm.me <ezri.munro@pm.me>
2026-01-25 14:05:03 +0000

    Now doesn't have an interrupt on single tap so hold is sent if another key is pressed while layer pressed down.

--- keyboards/tweetydabird/lotus58/keymaps/mardorien/keymap.c
+++ keyboards/tweetydabird/lotus58/keymaps/mardorien/keymap.c
@@ -460,8 +460,8 @@ bool oled_task_user(void) {
 
 td_state_t cur_dance(tap_dance_state_t *state) {
     if (state->count == 1) {
-        if (state->interrupted || !state->pressed) return TD_SINGLE_TAP;
-        else return TD_SINGLE_HOLD;
+        if (state->pressed) return TD_SINGLE_HOLD;
+        else return TD_SINGLE_TAP;
     } else if (state->count == 2) {
         if (state->pressed) return TD_DOUBLE_HOLD;
         else return TD_DOUBLE_TAP;
